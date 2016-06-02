7e57f0e5bcab55be6c3dbcab3a9e551ae14b68e7
Vijay <vjpscjp@gmail.com>
2016-06-02 16:06:03 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var serveStatic = require("serve-static");
 
 var app = express(); 
 app.use(serveStatic(__dirname + "/.")); 
-
+app.get('/host', function (req, res) {
+	var pg={}
+	pg.API 	= process.env.API_HOST
+	res.json(pg);
+});
 //Server listening port.
 app.listen(3000);
 console.log('Server running on http://0.0.0.0:3000/');
